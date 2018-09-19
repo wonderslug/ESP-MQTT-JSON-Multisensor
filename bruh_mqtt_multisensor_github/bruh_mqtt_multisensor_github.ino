c4115a01be33eea5bece3e32c3d5cfe13e137625
Brian Towles <brian@towles.com>
2018-09-19 09:27:01 -0500

    Revert "status"

--- bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
+++ bruh_mqtt_multisensor_github/bruh_mqtt_multisensor_github.ino
@@ -89,7 +89,7 @@ float humValue;
 
 int pirValue;
 int pirStatus;
-String motionStatus="standby";
+String motionStatus;
 
 char message_buff[100];
 
@@ -145,9 +145,6 @@ void setup() {
   pinMode(PIRPIN, INPUT);
   pinMode(DHTPIN, INPUT);
   pinMode(LDRPIN, INPUT);
-  pinMode(redPin,OUTPUT);
-  pinMode(greenPin,OUTPUT);
-  pinMode(bluePin,OUTPUT);
 
   Serial.begin(115200);
   delay(10);
@@ -348,14 +345,14 @@ void sendState() {
   color["g"] = green;
   color["b"] = blue;
 
+
   root["brightness"] = brightness;
+  root["humidity"] = (String)humValue;
   root["motion"] = (String)motionStatus;
   root["ldr"] = (String)LDR;
-  if (humValue>1) { //only transmit valid values
-    root["humidity"] = (String)humValue;
-    root["temperature"] = (String)tempValue;
-    root["heatIndex"] = (String)calculateHeatIndex(humValue, tempValue);
-  }
+  root["temperature"] = (String)tempValue;
+  root["heatIndex"] = (String)calculateHeatIndex(humValue, tempValue);
+
 
   char buffer[root.measureLength() + 1];
   root.printTo(buffer, sizeof(buffer));
